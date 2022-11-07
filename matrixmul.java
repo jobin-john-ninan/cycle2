1aeb96da5cbdd74dfa601d7db6410663692dad6a
jobin-john-ninan <115853792+jobin-john-ninan@users.noreply.github.com>
2022-11-07 14:07:57 +0530

    Update matrixmul.java

--- matrixmul.java
+++ matrixmul.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class matrixmul {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         Scanner obj = new Scanner (System.in);
 
